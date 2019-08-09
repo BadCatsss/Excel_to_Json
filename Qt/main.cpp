0ee00e7b1cf239ae9168556e7265434396c13ec0
BadCatss <VeryBadCatss@Gmail.com>
2019-08-09 18:04:27 +0300

    09.08.19

---
+++ Qt/main.cpp
@@ -0,0 +1,17 @@
+#include <QCoreApplication>
+
+#include "xlsxdocument.h"
+#include "xlsxchartsheet.h"
+#include "xlsxcellrange.h"
+#include "xlsxchart.h"
+#include "xlsxrichstring.h"
+#include "xlsxworkbook.h"
+#include <QDebug>
+using namespace QXlsx;
+
+int main(int argc, char *argv[])
+{
+
+
+     return 0;
+}
